1565b0107ca2553e79e836d2b1b444001fdd5c8c
John Ahn <smileajw1004@gmail.com>
2020-11-29 12:50:10 +0900

    dev-example added

---
+++ server/config/dev-example.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://name:password@cluster0-xtlkz.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientId: 'sd-rg9qsn12q6984i6frlbdsk40vvgtcgree.apps.googleusercontent.com',
+    googleClientSecret: 'dsdfsdfsdfsdfsdf',
+    cookieEncryptionKey: 'hello'
+}
